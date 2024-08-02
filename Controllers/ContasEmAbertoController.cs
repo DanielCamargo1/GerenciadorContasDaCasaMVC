58ff13f87afa0c4228bdd500ed9ea97b54a6b1f9
DanielCamargo <danidcp098@gmail.com>
2024-08-02 11:59:40 -0300

    mudanca no css do projeto todo

--- Controllers/ContasEmAbertoController.cs
+++ Controllers/ContasEmAbertoController.cs
@@ -26,6 +26,25 @@ namespace ContaHoueseMvc.Controllers
             return View();
         }
 
+        [HttpGet]
+
+        public IActionResult ExcluirConta(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var doacao = _context.Contas.FirstOrDefault(x => x.Id == id);
+
+            if (doacao == null)
+            {
+                return NotFound();
+            }
+            return View(doacao);
+
+        }
+
         [HttpPost]
         public IActionResult AdicionarConta(ContaModel conta)
         {
@@ -36,6 +55,19 @@ namespace ContaHoueseMvc.Controllers
             _context.Contas.Add(conta);
             _context.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConta( ContaModel conta)
+        {
+
+            if(conta != null)
+            {
+                _context.Contas.Remove(conta);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
         }
     }
 }
