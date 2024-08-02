75775fbc085b266dcff7c089fe577fc7024efb88
DanielCamargo <danidcp098@gmail.com>
2024-08-02 08:21:42 -0300

    migration

---
+++ Migrations/20240802111650_other.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContaHoueseMvc.Migrations
+{
+    public partial class other : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CódigoDeBarras",
+                table: "Contas");
+
+            migrationBuilder.RenameColumn(
+                name: "NomeConta",
+                table: "Contas",
+                newName: "NomeDaConta");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NomeDaConta",
+                table: "Contas",
+                newName: "NomeConta");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CódigoDeBarras",
+                table: "Contas",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
